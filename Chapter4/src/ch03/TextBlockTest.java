4778a95fc83140b00daea6c0163407bf2385b17c
woong99 <dndry1072@naver.com>
2022-07-02 19:36:32 +0900

    Chapter4

---
+++ Chapter4/src/ch03/TextBlockTest.java
@@ -0,0 +1,15 @@
+package ch03;
+
+public class TextBlockTest {
+
+	public static void main(String[] args) {
+
+		String textBlocks = """
+				Hello,
+				hi,
+				how r u
+				""";
+		System.out.println(textBlocks);
+	}
+
+}
