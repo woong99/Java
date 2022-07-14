0f6924d2147278970ae23962a0623761bcf20314
woong99 <dndry1072@naver.com>
2022-07-14 21:40:32 +0900

    ch10

---
+++ ch10/src/sorting/QuickSort.java
@@ -0,0 +1,19 @@
+package sorting;
+
+public class QuickSort implements Sort {
+    @Override
+    public void description() {
+        Sort.super.description();
+        System.out.println("QuickSort 입니다.");
+    }
+
+    @Override
+    public void ascending(int[] arr) {
+        System.out.println("QuickSort ascending");
+    }
+
+    @Override
+    public void descending(int[] arr) {
+        System.out.println("QuickSort descending");
+    }
+}
