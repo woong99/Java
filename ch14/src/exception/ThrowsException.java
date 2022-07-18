82c3be3ed5db1cd9055bcfbc1756523ca0740682
woong99 <dndry1072@naver.com>
2022-07-18 23:56:21 +0900

    ch14

---
+++ ch14/src/exception/ThrowsException.java
@@ -0,0 +1,17 @@
+package exception;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
+public class ThrowsException {
+    public Class loadClass(String fileName, String className) throws FileNotFoundException, ClassNotFoundException {
+        FileInputStream fis = new FileInputStream(fileName);
+        Class c = Class.forName(className);
+        return c;
+    }
+
+    public static void main(String[] args) throws FileNotFoundException, ClassNotFoundException {
+        ThrowsException test = new ThrowsException();
+        test.loadClass("a.txt", "java.lang.String");
+    }
+}
