b4bd07f81ddda6084de72962d665721efd792ddf
woong99 <dndry1072@naver.com>
2022-07-18 22:54:50 +0900

    ch12

---
+++ ch13/src/lambda/TestStringConcat.java
@@ -0,0 +1,15 @@
+package lambda;
+
+public class TestStringConcat {
+    public static void main(String[] args) {
+        String s1 = "Hello";
+        String s2 = "World!";
+        StringConcatImpl concat1 = new StringConcatImpl();
+        concat1.makeString(s1, s2);
+
+        StringConcat concat2 = (s, v) -> {
+            System.out.println(s + "," + v);
+        };
+        concat2.makeString(s1, s2);
+    }
+}
